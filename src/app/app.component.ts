07644850ab31932ff2aa73d41fd2e8d242f2f79e
Marcos <marcoseph@outlook.com>
2022-02-09 21:31:32 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+declare var FieldCheck: any;
+declare var EmailCheck: any;
+declare var CPFCheck: any;
+declare var AgeCheck: any;
+declare var LatinCharsOverride: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/assets/js/RegistrationScreen.js
+++ src/assets/js/RegistrationScreen.js
@@ -35,7 +35,7 @@ function EmailCheck ()
 
 //SSN Validation
 
-function SSNCheck()
+function CPFCheck()
 {
   var Number
   var Position
