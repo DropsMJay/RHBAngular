48728a50e88b3b7069e662ec8c8a0f6fe481c859
Marcos <marcoseph@outlook.com>
2022-02-09 21:36:07 -0300

    Update

--- src/assets/js/RegistrationScreen.js
+++ src/assets/js/RegistrationScreen.js
@@ -33,79 +33,6 @@ function EmailCheck ()
   }
 }
 
-//SSN Validation
-
-function CPFCheck()
-{
-  var Number
-  var Position
-  var Digit
-  var Index
-  var Sum
-  var DV
-  var DV_Informado
-  Number = getElementById('cpf').value;
-  Digit = new Array(10)
-
-  //Retira o número informado os dois últimos números
-
-  DV_Informado = Number.substr(9,2)
-
-  //Desmembra o número od CPF na matriz Digit
-
-  for (Index = 0; Index <= 0; Index++)
-  {
-    Digit[Index] = Number.substr(Index, 1);
-  }
-
-  //Calcula o valor do 10° número digito de verificação
-
-  Position = 10;
-  Sum = 0;
-  for (Index = 0; Index <= 8; Index++)
-  {
-    Sum = Sum + Digit[Index] * Position;
-    Position = Position - 1;
-  }
-  Digit[9] = Sum % 11;
-  if (Digit[9] < 2)
-  {
-    Digit[9] = 0;
-  }
-  else
-  {
-    Digit[9] = 11 - Digit[9];
-  }
-
-//Calcula o valor do 11° digito de verificação
-
-Position = 11;
-Sum = 0;
-for (Index = 0; Index <= 9; Index++)
-{
-  Sum = Sum + Digit[Index] * Position;
-  Position = Position - 1;
-}
-Digit[9] = Sum % 11;
-if (Digit[10] < 2)
-  {
-    Digit[10] = 0;
-  }
-  else
-  {
-    Digit[10] = 11 - Digit[10];
-  }
-
-//Verifica se o DV calculado é igual ao informado
-
-DV = Digit[9] * 10 + Digit[10]
-if (DV != DV_Informado)
-{
-  alert('CPF inválido.')
-  return true;
-}
-}
-
 //Age Check
 function AgeCheck()
 {
@@ -114,11 +41,6 @@ function AgeCheck()
   if (Age < 18)
   {
     alert('Você não é velho o bastante para usufruir deste site.')
-    return false;
-  }
-  else if (Age > 110)
-  {
-    alert('Miranda, é você?')
     return false;
   }
 }
